ca403c6f6abac3e202c4c8c98d7fcffadca6e3d7
MagdaBaszczynska1 <125397035+MagdaBaszczynska1@users.noreply.github.com>
2026-06-22 12:04:22 +0200

    feat: export history query and measurement form services

--- src/services/index.js
+++ src/services/index.js
@@ -3,3 +3,5 @@ export * from "./sag-slider-configuration.js";
 export * from "./calculator-profile-prefill.js";
 export * from "./sag-calculator.js";
 export * from "./bike-profile-form.js";
+export * from "./measurement-history-query.js";
+export * from "./measurement-form.js";
